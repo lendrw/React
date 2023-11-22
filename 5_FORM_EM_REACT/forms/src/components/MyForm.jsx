ee5915fb2d811045a744fba2379beb67710d6f75
Leandro Freire Cabral <88214669+lendrw@users.noreply.github.com>
2023-11-22 11:39:43 -0300

    .

---
+++ 5_FORM_EM_REACT/forms/src/components/MyForm.jsx
@@ -0,0 +1,58 @@
+import './MyForm.css';
+import { useState } from 'react';
+
+const MyForm = ({user}) => {
+    // 6 - controled inputs
+    // 3 - gerenciamento de dados
+    const [name, setName] = useState(user ? user.name : "");
+    const [email, setEmail] = useState(user ? user.email : "");
+
+    const handleName = (e) => {
+        setName(e.target.value);
+    };
+
+    //console.log(name);
+    //console.log(email);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Enviando o formulário");
+        console.log(name, email);
+
+        // 7 - limpando formulário
+        setName("");
+        setEmail("");
+    };
+
+  return (
+    <div>
+        {/* 5 - envio de form */}
+        {/* 1 - criação de form */}
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="name">Nome:</label>
+                <input 
+                type="text" 
+                name='name' 
+                placeholder='Digite seu nome' 
+                onChange={handleName}
+                value={name}/>
+            </div>
+            {/* 2 - label envolvendo input (sugerido) */}
+            <label>
+                <span>E-mail:</span>
+                {/* 4 - simplificação de manipulação de state */}
+                <input 
+                type="email" 
+                name="email" 
+                placeholder="Digite o seu e-mail" 
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}/>
+            </label>
+            <input type="submit" value={"Enviar"}/>
+        </form>
+    </div>
+  )
+}
+
+export default MyForm
