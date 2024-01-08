1848af8824ee55f7c373a616429e37be6bd6b61c
Leandro Freire Cabral <88214669+lendrw@users.noreply.github.com>
2024-01-08 15:28:21 -0300

    .

--- 7_REQUISIÇÕES_HTTP/httpreact/src/App.jsx
+++ 7_REQUISIÇÕES_HTTP/httpreact/src/App.jsx
@@ -1,12 +1,82 @@
-import './App.css'
+import './App.css';
+
+import { useState, useEffect } from "react";
+
+const url = "http://localhost:3000/products";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
+  // 1 - resgatando dados
+  useEffect(() => {
+
+    async function fetchData() {
+      const res = await fetch(url);
+
+      const data = await res.json();
+
+      setProducts(data);
+    }
+
+    fetchData();
+  }, []);
+
+  // 2 - adição de produtos
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const product = {
+      name,
+      price,
+    };
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(product)
+    });
+  };
 
   return (
-    <div>
-    <h1>Lista de produtos</h1>
+    <div className="App">
+      <h1>Lista de produtos</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - R$: {product.price}
+          </li>
+        ))}
+      </ul>
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input
+              type="text" 
+              value={name} 
+              name="name" 
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label>
+            Preço:
+            <input
+              type="number" 
+              value={price} 
+              name="price" 
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </label>
+          <input type="submit" value="Criar"/>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
