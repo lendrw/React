37f8e199f157ec746ea153a223e3d0cf59948efc
Leandro Freire <88214669+lendrw@users.noreply.github.com>
2024-05-11 05:35:52 -0300

    .

--- 8_REACT_ROUTER/react-router/src/App.jsx
+++ 8_REACT_ROUTER/react-router/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 
 // 1 - config react router
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 
 // components
 import Navbar from './components/Navbar';
@@ -34,6 +34,8 @@ function App() {
             <Route path="/products/:id/info" element={<Info/>}/>
             {/* 9 - search */}
             <Route path="/search" element={<Search/>}/>
+            {/* 10 - redirect */}
+            <Route path="/company" element={<Navigate to="/about"/>}/>
             {/* 7 - no match route */}
             <Route path="*" element={<NotFound/>}/>
           </Routes>
